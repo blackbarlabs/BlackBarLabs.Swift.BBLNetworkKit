ed34f78be4ebfc5220521c6a0f496e7ba829376e
Joel Perry <joel@joelperry.net>
2018-12-16 08:22:25 -0600

    New BBLError

--- BBLNetworkKit/Classes/BBLError.swift
+++ BBLNetworkKit/Classes/BBLError.swift
@@ -8,51 +8,36 @@
 
 import Foundation
 
-open class BBLError: Equatable, LocalizedError {
-    public var domain: String
-    public var code: Int
-    public var description: String
-    public var recoverySuggestion: String?
-    public var failureReason: String?
-    public var url: URL?
+public enum BBLError: LocalizedError {
+    case generic(String)
+    case invalidObject
+    case invalidRequest
+    case task(NSError)
+    case jsonDeserialize(NSError, Data?)
+    case urlResponse(URLResponse, Data?)
+    
+    public var code: Int? {
+        switch self {
+        case .generic, .invalidObject, .invalidRequest: return nil
+        case .jsonDeserialize(let error, _), .task(let error): return error.code
+        case .urlResponse(let response, _): return response.statusCode
+        }
+    }
+    
     public var localizedDescription: String {
-        return description
-    }
-    
-    public init(domain: String, code: Int, description: String,
-                recoverySuggestion: String? = nil, failureReason: String? = nil, url: URL? = nil) {
-        self.domain = domain
-        self.code = code
-        self.description = description
-        self.recoverySuggestion = recoverySuggestion
-        self.failureReason = failureReason
-    }
-    
-    public convenience init(errorObject: NSError) {
-        self.init(domain: errorObject.domain,
-                  code: errorObject.code,
-                  description: errorObject.localizedDescription,
-                  recoverySuggestion: errorObject.localizedRecoverySuggestion,
-                  failureReason: errorObject.localizedFailureReason)
-    }
-    
-    public convenience init(urlResponse: URLResponse) {
-        self.init(domain: "HTTPErrorDomain",
-                  code: urlResponse.statusCode,
-                  description: HTTPURLResponse.localizedString(forStatusCode: urlResponse.statusCode),
-                  recoverySuggestion: nil,
-                  failureReason: urlResponse.allHeaderFields["Reason"] as? String,
-                  url: urlResponse.url)
-    }
-    
-    public static func ==(lhs: BBLError, rhs: BBLError) -> Bool {
-        return lhs.domain == rhs.domain && lhs.code == rhs.code
+        switch self {
+        case .generic(let message): return message
+        case .invalidObject: return "Invalid object"
+        case .invalidRequest: return "Invalid request"
+        case .jsonDeserialize(let error, _), .task(let error): return error.localizedDescription
+        case .urlResponse(let response, _): return response.statusString
+        }
     }
 }
 
 public enum BBLResult<T> {
     case success(T)
-    case failure(BBLError)
+    case failure(Error)
     
     public func map<U>(_ f: (T) -> U) -> BBLResult<U> {
         switch self {
@@ -69,14 +54,14 @@ public enum BBLResult<T> {
             if let outValue = f(value) {
                 return .success(outValue)
             }
-            return .failure(BBLMappingError)
+            return .failure(BBLError.invalidObject)
             
         case .failure(let error):
             return .failure(error)
         }
     }
     
-    public func handle(success: (T) -> Void, failure: (BBLError) -> Void) {
+    public func handle(success: (T) -> Void, failure: (Error) -> Void) {
         switch self {
         case .success(let t):
             success(t)
@@ -87,21 +72,3 @@ public enum BBLResult<T> {
     }
 }
 
-// MARK: - Error Definitions
-public let BBLURLError = BBLError(domain: "BBLErrorDomain", code: 600, description: "Invalid URL")
-public let BBLInvalidObjectError = BBLError(domain: "BBLErrorDomain", code: 601, description: "Invalid object")
-public let BBLConnectionError = BBLError(domain: "BBLErrorDomain", code: 602, description: "No internet connection")
-
-public let BBLGETRequestError = BBLError(domain: "BBLErrorDomain", code: 610, description: "Bad GET request")
-public let BBLPUTRequestError = BBLError(domain: "BBLErrorDomain", code: 611, description: "Bad PUT request")
-public let BBLPATCHRequestError = BBLError(domain: "BBLErrorDomain", code: 612, description: "Bad PATCH request")
-public let BBLPOSTRequestError = BBLError(domain: "BBLErrorDomain", code: 613, description: "Bad POST request")
-public let BBLDELETERequestError = BBLError(domain: "BBLErrorDomain", code: 614, description: "Bad DELETE request")
-public let BBLOPTIONSRequestError = BBLError(domain: "BBLErrorDomain", code: 615, description: "Bad OPTIONS request")
-
-public let BBLNoDataError = BBLError(domain: "BBLErrorDomain", code: 630, description: "No data returned")
-public let BBLMappingError = BBLError(domain: "BBLErrorDomain", code: 631, description: "Object mapping error")
-
-public let BBLNoResponseError = BBLError(domain: "BBLErrorDomain", code: 640, description: "No response from server")
-
-public let BBLJSONError = BBLError(domain: "BBLErrorDomain", code: 650, description: "Unexpected JSON object")
